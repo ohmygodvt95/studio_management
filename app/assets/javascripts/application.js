8e892f66128e1a11e457d0ae557c55e75659fa06
ThienLV <ohmygodvt95@gmail.com>
2017-09-01 14:38:21 +0700

    Config layout site - powered by propeller.in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
-//= require_tree .
+//= require jquery
+//= require bootstrap/dist/js/bootstrap.min
+//= require propellerkit/dist/js/propeller.min
